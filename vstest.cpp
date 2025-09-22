7e5a6d37d05bcea45c6e9d7db6e2e1b1f3a76640
Hrittik Raushan <hrittikraushan9939@gmail.com>
2025-09-22 22:27:39 +0530

    initial commit

--- vstest.cpp
+++ vstest.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 int main(){
 
-    cout <<"kyu nhi ho rhi padhai";
+    cout <<"kyu nhi ho rhi padhai"<<endl;
+    cout <<"kya karu"<<endl;
+    cout <<"kuch to kar";
+    cout <<"\n";
+    cout <<"dekho bhai baat hai clear"<<endl;
+    cout <<"abhi to mazaa aa rha hai"<<endl;
+    cout <<"abhi to mazaa aa rha hai"<<"\n";
+    cout <<"utha ke patak denge ki chatak jayega"<<endl;
+
+    int x = 2, y = 3;
+    cout <<x+y<<endl;
+    cout <<x*y<<endl;
+    cout <<x-y<<endl;
+    cout <<x/y<<endl;
+    cout <<x%y<<endl;
     
+    
+
+
+
 }
