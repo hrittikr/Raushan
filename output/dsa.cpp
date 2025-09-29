2b6112d4fee425083e7473b9abdc91c37a79e103
Hrittik Raushan <hrittikraushan9939@gmail.com>
2025-09-29 14:42:48 +0530

    initial commit

--- output/dsa.cpp
+++ output/dsa.cpp
@@ -4,9 +4,17 @@
 using namespace std;
 
 int main ()
+// {
+//   int a, b=3;
+//   a = b;
+//   a+=2;             // equivalent to a=a+2
+//   cout << a;
+
+// declare variables
 {
-  int a, b=3;
-  a = b;
-  a+=2;             // equivalent to a=a+2
-  cout << a;
+int a =12 ,  b = 25;
+ cout <<"a="<<a<<endl;
+ cout <<"b="<<b<<endl;
+ cout <<"a&b = "<<(a&b)<<endl;   
+  return 0;
 }
